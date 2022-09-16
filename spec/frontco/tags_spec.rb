d9060c80258c97093e47809cbfb0dc4bb7296f2f
JajaDoods <bloorrare@proton.me>
2022-09-16 10:37:24 +0300

    Test for Frontco::Tags

---
+++ spec/frontco/tags_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Frontco::Tags do
+  it 'Has a paired tags' do
+    expect(Frontco::Tags::PAIRED_TAGS).not_to be nil
+  end
+
+  it 'Has a singleton tags' do
+    expect(Frontco::Tags::SINGLETON_TAGS).not_to be nil
+  end
+end

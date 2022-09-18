9bb7053e90912f87de8eb371992453bf8a21da6e
JajaDoods <bloorrare@proton.me>
2022-09-18 21:19:00 +0300

    Moved render connector

--- lib/frontco/renders.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Frontco
-  module Renders
-    autoload(:HypertextRender, 'frontco/renders/hypertext_render.rb')
-    autoload(:HTMLRender, 'frontco/renders/html_render.rb')
-    autoload(:PugRender, 'frontco/renders/pug_render.rb')
-  end
-end

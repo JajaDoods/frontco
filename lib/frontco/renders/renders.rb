6f452022a0b051901f261038fc1090bc18414ead
JajaDoods <bloorrare@proton.me>
2022-09-18 21:18:19 +0300

    Move connector

---
+++ lib/frontco/renders/renders.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Frontco
+  module Renders
+    autoload(:HypertextRender, 'frontco/renders/hypertext_render.rb')
+    autoload(:HTMLRender, 'frontco/renders/html_render.rb')
+  end
+end

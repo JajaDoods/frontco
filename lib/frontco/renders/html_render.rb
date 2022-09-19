abcf1712a486b143db936c195d600e0648a33173
JajaDoods <bloorrare@proton.me>
2022-09-19 22:22:37 +0300

    Indentified strong connectivity between tags

--- lib/frontco/renders/html_render.rb
+++ lib/frontco/renders/html_render.rb
@@ -25,7 +25,7 @@ module Frontco
       end
 
       def add_tag(tag, paired, *text, **attrs, &subtags)
-        tag = HTMLTag.new(tag, paired, *text, **attrs)
+        tag = HTMLTag.new(tag, paired, @parent_tag, *text, **attrs)
 
         unless paired && block_given?
           @parent_tag.is_a?(HTMLTag) ? @parent_tag << tag : @tags << tag

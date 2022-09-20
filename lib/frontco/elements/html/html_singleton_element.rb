3e0a9bd5b144be71bcf80bba07cef64a9606680a
JajaDoods <bloorrare@proton.me>
2022-09-20 22:40:20 +0300

    Added class that represents singleton html tags

---
+++ lib/frontco/elements/html/html_singleton_element.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Frontco
+  module Elements
+    module HTML
+      # Class that represents singleton HTML tag
+      class HTMLSingletonElement < HTMLElement
+        def initialize(tag, **attrs)
+          @tag = tag
+          @attrs = attrs
+        end
+
+        def render(**params)
+          @indent = params.fetch(:indent, 0)
+
+          formatted_attrs = @attrs.empty? ? '' : " #{format_attrs}"
+          add_indents("<#{@tag}#{formatted_attrs}>\n")
+        end
+      end
+    end
+  end
+end

11ac1879900ca062784c63d25ac5f68db0358b49
JajaDoods <bloorrare@proton.me>
2022-09-20 22:34:54 +0300

    Added HTML autoloader

---
+++ lib/frontco/elements/html/html.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Frontco
+  module Elements
+    # HTML module contains HTML elements
+    module HTML
+      autoload(:HTMLElement, 'frontco/elements/html/html_element.rb')
+      autoload(:HTMLDoctypeElement, 'frontco/elements/html/html_doctype_element.rb')
+      autoload(:HTMLPairedElement, 'frontco/elements/html/html_paired_element.rb')
+      autoload(:HTMLSingletonElement, 'frontco/elements/html/html_singleton_element.rb')
+
+      # Error for unkown HTML tag
+      class UnknownHTMLTag < ArgumentError
+        def initialize(tag)
+          super("Unkown tag #{tag}")
+        end
+      end
+
+      def self.create_element(tag, *text, **attrs)
+        include Frontco::Atoms::HypertextAtoms
+
+        case tag
+        when :doctype then HTMLDoctypeElement.new
+        when *PAIRED_TAGS then HTMLPairedElement.new(tag, *text, **attrs)
+        when *SINGLETON_TAGS then HTMLSingletonElement.new(tag, **attrs)
+        else
+          raise UnknownHTMLTag, tag
+        end
+      end
+    end
+  end
+end

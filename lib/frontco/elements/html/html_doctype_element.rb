a9e2678e26aacab4343d865164c5765affd401f3
JajaDoods <bloorrare@proton.me>
2022-09-20 22:40:49 +0300

    Added class that represents html doctype tag

---
+++ lib/frontco/elements/html/html_doctype_element.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Frontco
+  module Elements
+    module HTML
+      # Class that represents the doctype HTML tag
+      class HTMLDoctypeElement < HTMLElement
+        def render(**params)
+          @indent = params.fetch(:indent, 0)
+
+          add_indents("<!DOCTYPE html>\n")
+        end
+      end
+    end
+  end
+end

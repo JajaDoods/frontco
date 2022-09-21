7111f803d3a430b1161c1d7ca6ccca19f8927219
JajaDoods <bloorrare@proton.me>
2022-09-21 12:28:57 +0300

    Added PugRender

---
+++ lib/frontco/renders/pug_render.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module Frontco
+  module Renders
+    # Strategy for Pug rendering
+    class PugRender < HypertextRender
+      attr_reader :output
+
+      def initialize(&block)
+        @tags = []
+        @output = ''
+        @parent_tag = nil
+
+        new(&block)
+      end
+
+      def new(&block)
+        @tags.clear
+        @output = ''
+
+        @block = block
+        @block.arity.zero? ? instance_eval(&block) : yield(self)
+        self
+      end
+
+      def add_tag(tag, *text, **attrs, &subelements)
+        tag = Frontco::Elements::Pug.create_element(tag, *text, **attrs, &subelements)
+
+        if tag.is_a?(Frontco::Elements::Pug::PugPairedElement) && block_given?
+          parent_tag_copy = @parent_tag
+          @parent_tag = tag
+          subelements.arity.zero? ? instance_eval(&subelements) : yield(self)
+          @parent_tag = parent_tag_copy
+        end
+
+        @parent_tag.nil? ? @tags << tag : @parent_tag << tag
+      end
+
+      def render(**params)
+        @output = @tags.map do |tag|
+          tag.render(**params)
+        end.join
+        self
+      end
+
+      def to_file(file_path, mode: 'a')
+        File.write(file_path, @output, mode: mode)
+      end
+    end
+  end
+end
